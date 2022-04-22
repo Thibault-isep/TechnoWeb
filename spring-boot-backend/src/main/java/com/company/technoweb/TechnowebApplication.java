32149a4c15726b889badffd4d527fa7635abace3
nkrishan06 <74047608+nkrishan06@users.noreply.github.com>
2022-04-22 11:55:14 +0200

    first commit

---
+++ spring-boot-backend/src/main/java/com/company/technoweb/TechnowebApplication.java
@@ -0,0 +1,13 @@
+package com.company.technoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnowebApplication.class, args);
+	}
+
+}
---
+++ spring-boot-backend/src/test/java/com/company/technoweb/TechnowebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.technoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnowebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
