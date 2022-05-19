5f9edabba7c7552510ce6a806b151a2fff5d6c7e
BarthelemyEst <74047314+BarthelemyEst@users.noreply.github.com>
2022-05-19 15:35:21 +0200

    Delete src directory

--- src/main/java/fr/isep/homeExchange/repository/ReservationRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.isep.homeExchange.repository;
-
-import fr.isep.homeExchange.model.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
-}
